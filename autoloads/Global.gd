2b1b04d32bf4cfc17fcb3d8f343ee867b256d118
kaushalmeena <kaushal.meena1996@gmail.com>
2022-08-15 19:08:20 +0530

    style: format scripts and annotate types

--- autoloads/Global.gd
+++ autoloads/Global.gd
@@ -1,23 +1,27 @@
 extends Node
 
-var score = 0
-var best_score = 0
+var score: int = 0
+var best_score: int = 0
 
 var current_scene = null
 
-func _ready():
-	init_current_scene()
-	
-func init_current_scene():
-	var root = get_tree().root
-	current_scene = root.get_child(root.get_child_count() - 1)
-	
-func goto_scene(path):
+
+func goto_scene(path: String):
 	call_deferred("_deferred_goto_scene", path)
-	
-func _deferred_goto_scene(path):
+
+
+func _deferred_goto_scene(path: String):
 	current_scene.free()
-	var scene = ResourceLoader.load(path)
+	var scene: PackedScene = ResourceLoader.load(path)
 	current_scene = scene.instance()
 	get_tree().root.add_child(current_scene)
 	get_tree().current_scene = current_scene
+
+
+func _init_current_scene() -> void:
+	var root: Node = get_tree().root
+	current_scene = root.get_child(root.get_child_count() - 1)
+
+
+func _ready() -> void:
+	_init_current_scene()
--- player/Marble.gd
+++ player/Marble.gd
@@ -1,46 +1,46 @@
 extends KinematicBody
 
-const GRAVITY = 2;
-const MOVE_SPEED = 5
-const JUMP_SPEED = 30
+const GRAVITY: float = 2.0
+const MOVE_SPEED: float = 5.0
+const JUMP_SPEED: float = 30.0
 
-const LANES = [-10, -5,  0, 5, 10]
+const LANES: Array = [-10, -5, 0, 5, 10]
 
-var velocity = Vector3.ZERO
-var distance = 0;
-var lane_cnt = LANES.size()
-var lane_idx = 2;
+var lane_cnt: int = LANES.size()
+var lane_idx: int = 2
 
-func _ready():
-	pass # Replace with function body.
-	
-func _get_input():
-	if Input.is_action_just_pressed('ui_left') and lane_idx > 0:
+var velocity: Vector3 = Vector3.ZERO
+
+
+func _ready() -> void:
+	pass
+
+
+func _get_input() -> void:
+	if Input.is_action_just_pressed("ui_left") and lane_idx > 0:
 		lane_idx -= 1
-	elif Input.is_action_just_pressed('ui_right') and lane_idx < lane_cnt - 1:
+	elif Input.is_action_just_pressed("ui_right") and lane_idx < lane_cnt - 1:
 		lane_idx += 1
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y += JUMP_SPEED
 
-func _check_collision():
-	if (get_slide_count() > 1):
-		Global.goto_scene('res://menus/DeathMenu.tscn')
 
-func _move():
-	distance = LANES[lane_idx] - global_transform.origin.x
-	if (abs(distance) > 0.1):
+func _check_collision() -> void:
+	if get_slide_count() > 1:
+		Global.goto_scene("res://menus/DeathMenu.tscn")
+
+
+func _move() -> void:
+	var distance: float = LANES[lane_idx] - global_transform.origin.x
+	if abs(distance) > 0.1:
 		velocity.x = distance * MOVE_SPEED
 	else:
 		velocity.x = 0
 	velocity.y -= GRAVITY
 	velocity = move_and_slide(velocity, Vector3.UP)
 
-func _physics_process(_delta):
+
+func _physics_process(_delta: float) -> void:
 	_get_input()
 	_move()
 	_check_collision()
-	
-	
-		
-		
-	
